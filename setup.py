0f632899249ee8f5ef1f70ed4fe01a4fb252f662
Rob Beagrie <rob@beagrie.com>
2013-08-19 12:51:33 +0100

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wordpress_api_models',
+      version='0.1',
+      description='Models to access a wordpress hosted blog through the JSON API',
+      author='Rob Beagrie',
+      author_email='rob@beagrie.com',
+      url='https://github.com/rbeagrie/django-wordpress-api-models',
+      packages=['wordpress_api_models'],
+      install_requires=['xml_models','json_models','common_models']
+     )
